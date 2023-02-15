922041c2f00d99a5c8c4e7ae7409dd07e46574a9
lidor ettinger <lidor.ettinger@gmail.com>
2023-02-15 11:17:53 +0200

    Pipeline improvement 2

--- Jenkins/pipelines/Cron.groovy
+++ Jenkins/pipelines/Cron.groovy
@@ -13,7 +13,7 @@ pipeline {
     stages {
         stage('Do job stage') {
             steps {
-                git branch: params.branch, url: 'https://github.com/naturalett/webhook-test.git'
+                git branch: params.branch, url: 'https://github.com/naturalett/continuous-integration.git'
             }
         }
     }
